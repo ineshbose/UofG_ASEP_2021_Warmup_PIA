5ed58026e08605a9d8819ecc3261ab1f5c96c004
Aidanddow <2468915D@student.gla.ac.uk>
2021-11-03 15:09:17 +0000

    Removed Folder 2468915D

--- 2468915D/kata_2.py
+++
@@ -1,9 +0,0 @@
-def spin_words(sentence):
-    new_sentence = []
-    for word in sentence.split():
-        if len(word) >= 5:
-            new_sentence.append(word[::-1])
-        else:
-            new_sentence.append(word)
-
-    return " ".join(new_sentence)
